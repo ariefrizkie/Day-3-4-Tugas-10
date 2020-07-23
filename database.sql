433a986c56c0ab8e07d6ba046ed3d3547c22601d
ariefrizkie <67990052+ariefrizkie@users.noreply.github.com>
2020-07-23 12:15:45 +0800

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,1 +1,10 @@
+/* Create Database and Table */
+create database arcademy;
 
+CREATE TABLE produk (
+  id_produk int not null auto_increment primary key,
+  nama_produk varchar(100),
+  keterangan varchar(100),
+  harga varchar(15),
+  jumlah char(15),
+);
